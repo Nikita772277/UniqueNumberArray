c6bea9485c6f0f33464dd6293a08690d5269fab2
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-12-05 11:13:57 +0300

    Добавьте файлы проекта.

---
+++ UniqueNumberArray/Program.cs
@@ -0,0 +1,5 @@
+﻿using UniqueNumberArray;
+
+Work work=new ();
+int[] ints=new int[7] {3,1,1,1,1,1,1};
+work.DifferingNumber(ints);
---
+++ UniqueNumberArray/Work.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueNumberArray
+{
+    public class Work
+    {
+        public void DifferingNumber(int[] ints)
+        {
+            if (ints.Length % 2 == 0)
+            {
+
+            }
+            else
+            {
+                int numberDiffering = 0;
+                for (int i = 0; i < ints.Length; i++)
+                {
+                    int counter = 0;
+                    foreach (int c in ints)
+                    {
+                        if (ints[i] == c)
+                        {
+                            counter++;
+                        }
+                    }
+                    if (counter == 1)
+                    {
+                        numberDiffering = ints[i];
+                    }
+                }
+                Console.WriteLine(numberDiffering);
+            }
+        }
+    }
+}
